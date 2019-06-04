240dafa6187813c8b62c0eafc21b3843fb61664f
ViktorVojtek <vvojtek@enli.sk>
2019-06-04 14:05:42 +0200

    Work in progress

--- src/public/tsx/components/Orders/components/MultiSelectCheckBox.tsx
+++ src/public/tsx/components/Orders/components/MultiSelectCheckBox.tsx
@@ -8,6 +8,7 @@ interface IProps {
 }
 interface IState {
   selectOpened: boolean
+  selectedProducts: object[]
 }
 interface IProduct {
   _id: string
@@ -22,10 +23,45 @@ class MultiSelectCheckBox extends React.Component<IProps, IState> {
 
     this.state = {
       selectOpened: false,
+      selectedProducts: [],
     };
 
     this.handleSelectOpen = this.handleSelectOpen.bind(this);
     this.handleWatchClick = this.handleWatchClick.bind(this);
+  }
+
+  handlePopulateSelectdProducts() {
+    const { itemNum, orders, products } = this.props;
+    const order = orders[itemNum];
+    let arr: object[] = [];
+
+    if ((order as any).products.length > 0) {
+      for (let i = 0; i < products.length; i++) {
+        let state = {
+          _id: products[i]._id,
+          checked: false,
+        };
+
+        if ((order as any).products[i] && (order as any).products[i]._id === products[i]._id) {
+          state = {
+            _id: products[i]._id,
+            checked: true,
+          };
+        }
+
+        arr.push(state);
+      }
+    } else {
+      for (let i = 0; i < products.length; i++) {
+        arr.push({
+          _id: products[i]._id,
+          checked: false,
+        });
+      }
+    }
+    
+
+    this.setState({ selectedProducts: arr });
   }
 
   handleSelectOpen() {
@@ -44,6 +80,7 @@ class MultiSelectCheckBox extends React.Component<IProps, IState> {
 
   componentDidMount() {
     document.addEventListener('mousedown', this.handleWatchClick);
+    this.handlePopulateSelectdProducts();
   }
 
   render() {
@@ -54,28 +91,31 @@ class MultiSelectCheckBox extends React.Component<IProps, IState> {
       <div className="multi-select-checkbox-wrapper">
         <button
           onClick={this.handleSelectOpen}
-          className=""
         >Vybrať {selectOpened ? '▲' : '▼'}</button>
         <ul className={`multi-select-checkbox ${selectOpened ? 'open' : ''}`}>
           {
             (products && products.length > 0) && products.map(({ _id, code, price, title }, k: number) => {
-              (orders[itemNum] as any)
-              
+              // (orders[itemNum] as any)
               return (
                 <li key={_id} className="multi-select-checkbox-item">
                   <input
                     type="checkbox"
                     id={`prod-${_id}`}
                     onChange={() => {
+                      const { selectedProducts } = this.state;
                       const orderToUpdate = orders[itemNum];
                       const product = { _id, code, price, title };
+
+                      const checkedProdut = selectedProducts.filter(item => (item as any)._id === _id).pop();
+                      const { checked } = checkedProdut;
+                      console.log(checked);
   
                       if ((orderToUpdate as any).products) {
                         (orderToUpdate as any).products.push(product);
                       } else {
                         (orderToUpdate as any).products = [product];
                       }
-                      
+
                       updateItem(orderToUpdate);
                     }}
                   />
