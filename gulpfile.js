e99717c51c646e147ada9c7a1d0bfa26751923ce
ViktorVojtek <viktor1vojtek@gmail.com>
2020-06-28 19:05:27 +0200

    Updated gulpfile to match gulp v4 needs

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,19 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var uncss = require('gulp-uncss');
 
-gulp.task('minify-js', function() {
-  var stream = gulp.src('./src/public/js/*.js').pipe(uglify()).pipe(rename({ extname: '.min.js' })).pipe(gulp.dest('./dist/public/js/'));
+const { series } = gulp;
+
+function minifyJs() {
+  var stream = gulp
+    .src('./src/public/js/*.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./dist/public/js/'));
   return stream;
-});
+}
 
-gulp.task('watch:js', function() {
-  gulp.watch('./src/public/js/*.js', ['default']);
-});
+function watchJs() {
+  gulp.watch(['./src/public/js/*.js']);
+}
 
-gulp.task('uncss:css', function() {
-  return gulp.src('./dist/public/css/bootstrap.min.css').pipe(uncss({
-    html: ['https://carwellness.codebrothers.sk', 'https://carwellness.codebrothers.sk/sluzby', 'https://carwellness.codebrothers.sk/cennik', 'https://carwellness.codebrothers.sk/galeria', 'https://carwellness.codebrothers.sk/kontakt', 'https://carwellness.codebrothers.sk/reklamacia', 'https://carwellness.codebrothers.sk/objednavka']
-  })).pipe(gulp.dest('./dist/public/css'));
-});
-
-gulp.task('default', ['minify-js', 'watch:js']); // 'uncss:css'
+exports.default = series(minifyJs, watchJs);
